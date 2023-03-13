f56fcf99fefbfb86c902996fc7c0edd838cc0746
Linden Hamer <linden@mynethub.com>
2023-03-13 16:45:45 +0000

    rendering dummy post data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import { Fragment } from "react"
 import Hero from "../components/home-page/hero"
+import FeaturedPosts from "../components/home-page/featured-posts"
+
+const Dummy_Posts = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Geting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is the React framework for production. It makes building fullstack React sites or apps a breeze and ships with built in Server Side Rendering (SSR) ",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Geting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is the React framework for production. It makes building fullstack React sites or apps a breeze and ships with built in Server Side Rendering (SSR) ",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Geting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is the React framework for production. It makes building fullstack React sites or apps a breeze and ships with built in Server Side Rendering (SSR) ",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Geting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJs is the React framework for production. It makes building fullstack React sites or apps a breeze and ships with built in Server Side Rendering (SSR) ",
+    date: "2022-02-10",
+  },
+]
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={Dummy_Posts} />
     </Fragment>
   )
 }
