a29ddec63936ed01d3a5e28ae3ef0eea07a5cd8d
Linden Hamer <linden@mynethub.com>
2023-03-23 15:47:10 +0000

    missed save

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, Nextscript } from "next/document "
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <body>
           <Main />
           <Nextscript />
